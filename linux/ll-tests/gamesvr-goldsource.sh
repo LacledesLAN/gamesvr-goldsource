965d3d3927da0856e141470d2b0c39c6f1ed20f6
James Dudley <dudleycodes@users.noreply.github.com>
2018-09-13 19:27:37 -0500

    adding tests

--- linux/ll-tests/gamesvr-goldsource.sh
+++ linux/ll-tests/gamesvr-goldsource.sh
@@ -1,1 +1,46 @@
-echo placeholder file
+#!/bin/bash -i
+
+echo -ne "\n";
+echo -ne "  ─────────────────────────────▄██▄ \n"; sleep 0.018;
+echo -ne "  ─────────────────────────────▀███ \n"; sleep 0.018;
+echo -ne "  ────────────────────────────────█ \n"; sleep 0.018;
+echo -ne "  ───────────────▄▄▄▄▄────────────█ \n"; sleep 0.018;
+echo -ne "  ──────────────▀▄────▀▄──────────█ \n"; sleep 0.018;
+echo -ne "  ──────────▄▀▀▀▄─█▄▄▄▄█▄▄─▄▀▀▀▄──█ \n"; sleep 0.018;
+echo -ne "  ─────────█──▄──█────────█───▄─█─█ \n"; sleep 0.018;
+echo -ne "  ─────────▀▄───▄▀────────▀▄───▄▀─█ \n"; sleep 0.018;
+echo -ne "  ──────────█▀▀▀────────────▀▀▀─█─█ \n"; sleep 0.018;
+echo -ne "  ──────────█───────────────────█─█ \n"; sleep 0.018;
+echo -ne "  ▄▀▄▄▀▄────█──▄█▀█▀█▀█▀█▀█▄────█─█ \n"; sleep 0.018;
+echo -ne "  █▒▒▒▒█────█──█████████████▄───█─█ \n"; sleep 0.018;
+echo -ne "  █▒▒▒▒█────█──██████████████▄──█─█ \n"; sleep 0.018;
+echo -ne "  █▒▒▒▒█────█───██████████████▄─█─█ \n"; sleep 0.018;
+echo -ne "  █▒▒▒▒█────█────██████████████─█─█ \n"; sleep 0.018;
+echo -ne "  █▒▒▒▒█────█───██████████████▀─█─█ \n"; sleep 0.018;
+echo -ne "  █▒▒▒▒█───██───██████████████──█─█ \n"; sleep 0.018;
+echo -ne "  ▀████▀──██▀█──█████████████▀──█▄█ \n"; sleep 0.018;
+echo -ne "  ──██───██──▀█──█▄█▄█▄█▄█▄█▀──▄█▀  \n"; sleep 0.018;
+echo -ne "  ──██──██────▀█─────────────▄▀▓█   \n"; sleep 0.018;
+echo -ne "  ──██─██──────▀█▀▄▄▄▄▄▄▄▄▄▀▀▓▓▓█   \n"; sleep 0.018;
+echo -ne "  ──████────────█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█   \n"; sleep 0.018;
+echo -ne "  ──███─────────█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█   \n"; sleep 0.018;
+echo -ne "  ──██──────────█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█   \n"; sleep 0.018;
+echo -ne "  ──██──────────█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█   \n"; sleep 0.018;
+echo -ne "  ──██─────────▐█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█   \n"; sleep 0.018;
+echo -ne "  ──██────────▐█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█   \n"; sleep 0.018;
+echo -ne "  ──██───────▐█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▌   \n"; sleep 0.018;
+echo -ne "  ──██──────▐█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▌    \n"; sleep 0.018;
+echo -ne "  ──██─────▐█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▌     \n"; sleep 0.018;
+echo -ne "  ──██────▐█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▌      \n"; sleep 0.018;
+echo -ne "\n";
+echo -ne "This is just a convenience script - do not use for CI pipelines!\n";
+echo -ne "\n";
+
+/app/ll-tests/gamesvr-goldsource.cstrike.sh
+/app/ll-tests/gamesvr-goldsource.czero.sh
+/app/ll-tests/gamesvr-goldsource.dmc.sh
+/app/ll-tests/gamesvr-goldsource.dod.sh
+/app/ll-tests/gamesvr-goldsource.gearbox.sh
+/app/ll-tests/gamesvr-goldsource.hldm.sh
+/app/ll-tests/gamesvr-goldsource.ricochet.sh
+/app/ll-tests/gamesvr-goldsource.tfc.sh
